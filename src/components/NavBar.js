5e95b39454fe5637f31985ff844e64260c669dec
Danlev4 <danieleonva4@gmail.com>
2022-06-06 12:40:44 -0500

    nav

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import "./NavBar.css";
+export default function NavBar() {
+    return (
+        <nav className=" nav-main">
+            <ul>
+                <li>Categoría 1</li>
+                <li>Categoría 2</li>
+            </ul>
+        </nav>
+    );
+  }
+  
