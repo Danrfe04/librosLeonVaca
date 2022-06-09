83cf7159750cf48948110611f723131007225e43
Danlev4 <danieleonva4@gmail.com>
2022-06-08 20:45:52 -0500

    components

---
+++ src/components/CarWidget.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CarWidget() {
+  return (
+    <div>
+        <img src="../img/car.png" alt="carro de compras"/>
+    </div>
+  )
+}
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,13 @@
 import "./NavBar.css";
+import NavLink from "./NavLink/NavLink";
 export default function NavBar() {
     return (
         <nav className=" nav-main">
             <ul>
+                <NavLink/>
                 <li>Categoría 1</li>
                 <li>Categoría 2</li>
+                <CarWidget/>
             </ul>
         </nav>
     );
